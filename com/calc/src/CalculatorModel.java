020d7330b7a906da0d355aa812ef19e6679bd728
dokxid <dan-du@live.de>
2022-06-30 13:11:34 +0200

    change CalculatorModel methods to non-static

--- com/calc/src/CalculatorModel.java
+++ com/calc/src/CalculatorModel.java
@@ -3,36 +3,36 @@ package com.calc.src;
 import java.math.*;
 
 public class CalculatorModel {
-    public static double add(double x1, double x2){
+    public double add(double x1, double x2){
         return  x1+x2;
     }
-    public static double substract(double x1, double x2){
+    public double substract(double x1, double x2){
         return  x1-x2;
     }
-    public static double divide(double x1, double x2){
+    public double divide(double x1, double x2){
         return  x1/x2;
     }
-    public static double multiply(double x1, double x2){
+    public double multiply(double x1, double x2){
         return  x1*x2;
     }
-    public static double power(double x1, int n){
+    public double power(double x1, int n){
         double tempX = 1;
         for (int i = 0; i < n; i++) {
             tempX *= x1;
         }
         return  tempX;
     }
-    public static double squareRoot(double x1){
+    public double squareRoot(double x1){
         return Math.sqrt(x1);
     }
-    public static double nRoot(double x1, double x2){
+    public double nRoot(double x1, double x2){
         return Math.pow(x1,1/x2);
     }
 
-    public static void main(String[] args) {
-        System.out.println(add(5,5));
-        System.out.println(power(2,2));
-        System.out.println(squareRoot(5));
-        System.out.println(nRoot(27,3));
-    }
+//    public static void main(String[] args) {
+//        System.out.println(add(5,5));
+//        System.out.println(power(2,2));
+//        System.out.println(squareRoot(5));
+//        System.out.println(nRoot(27,3));
+//    }
 }
