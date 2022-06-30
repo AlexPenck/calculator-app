ba55e07bc909471d44df1a314b3832328c59932e
dokxid <dan-du@live.de>
2022-06-30 14:33:25 +0200

    add tests for subtract method

--- com/calc/tests/CalculatorModelTest.java
+++ com/calc/tests/CalculatorModelTest.java
@@ -2,9 +2,6 @@ package com.calc.tests;
 
 // importing junit stuff
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-
-import jdk.jfr.Category;
 import org.junit.jupiter.api.Test;
 
 // importing model to test
@@ -53,7 +50,7 @@ class CalculatorModelTest {
         assertEquals(-6.2, calc.add(-3.15,-3.05));
 
         // result with negative value (doubles)
-        assertEquals(-2.8, calc.add(10.3,-12.5));
+        assertEquals(-2.2, calc.add(10.3,-12.5));
 
         // test random doubles (precision x*(e-02))
         double randomDouble1;
@@ -67,4 +64,55 @@ class CalculatorModelTest {
 
     }
 
+
+    @Test
+    void subtract_int_values_test() {
+
+        // normal subtraction
+        assertEquals(11, calc.substract(16,5));
+
+        // subtraction with 0
+        assertEquals(10, calc.substract(10,0));
+
+        // subtraction with negative value
+        assertEquals(13, calc.substract(10,-3));
+
+        // subtraction with 2 negative values
+        assertEquals(1, calc.substract(-3,-4));
+
+        // result with negative value
+        assertEquals(-2, calc.substract(10,12));
+
+    }
+
+
+    @Test
+    void subtract_double_values_test() {
+
+        // normal subtraction (doubles)
+        assertEquals(2.75, calc.substract(9.25,6.5));
+
+        // subtraction with 0 (doubles)
+        assertEquals(10.25, calc.substract(10.25,0));
+
+        // subtraction with negative value (doubles)
+        assertEquals(13.4, calc.substract(10.3,-3.1));
+
+        // subtraction with 2 negative values (doubles)
+        assertEquals(-0.1, calc.substract(-3.15,-3.05));
+
+        // result with negative value (doubles)
+        assertEquals(-2.2, calc.substract(10.3,12.5));
+
+        // test random doubles (precision x*(e-02))
+        double randomDouble1;
+        double randomDouble2;
+        for (int i = 0; i < 25; i++) {
+            randomDouble1 = (Math.floor((Math.random()*1000-500))/10);
+            randomDouble2 = (Math.floor((Math.random()*1000-500))/10);
+            assertEquals((randomDouble1*10 - randomDouble2*10)/10,
+                    calc.substract(randomDouble1, randomDouble2));
+        }
+
+    }
 }
